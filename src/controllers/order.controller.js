392c95d31a0bfb9eb965c498841b0419bbaff960
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-04-18 11:36:21 +0530

    removed ordrs logs

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -56,7 +56,6 @@ export async function sendOrderNotification({
 
       try {
         await admin.messaging().send(userMessage);
-        console.log("User notification sent");
       } catch (error) {
         console.error("Error sending user notification:", error);
       }
@@ -81,7 +80,6 @@ export async function sendOrderNotification({
 
       try {
         await admin.messaging().send(adminMessage);
-        console.log("Admin notification sent");
       } catch (error) {
         console.error("Error sending admin notification:", error);
       }
