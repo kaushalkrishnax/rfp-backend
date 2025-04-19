787258eb36ebe536127af0b7cc181bbfa682aebe
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-04-19 21:37:55 +0530

    Added origin for ionic dev server

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "http://192.168.1.3:3000",
+  "http://192.168.1.3:8100",
   "https://royalfoodplaza.vercel.app",
   "https://localhost",
   "capacitor://localhost"
