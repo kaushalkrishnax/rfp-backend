0aaee0031a328adf9618f9e2b3fe6767ba85890b
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-04-21 11:01:05 +0530

    Added web.app origin

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "http://192.168.1.3:3000",
-  "http://192.168.1.3:8100",
+  "http://127.0.0.1:8100",
   "https://royalfoodplaza.vercel.app",
   "https://royalfoodplaza.web.app",
   "https://app.rfp.com",
